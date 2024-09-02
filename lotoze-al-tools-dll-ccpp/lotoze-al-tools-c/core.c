f5123557e70e08db6f534ac4175bf2d8b73eee65
lotoze <lotoze@163.com>
2024-09-02 19:02:18 +0800

    init

---
+++ lotoze-al-tools-dll-ccpp/lotoze-al-tools-c/core.c
@@ -0,0 +1,18 @@
+﻿// lotoze-al-tools-c.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "core.h"
+
+
+// 这是导出变量的一个示例
+LOTOZEALTOOLSCORE_API int nlotozealtoolsc=0;
+
+// 这是导出函数的一个示例。
+LOTOZEALTOOLSCORE_API int fnlotozealtoolsc(void)
+{
+    return 0;
+}
+
+
---
+++ lotoze-al-tools-dll-ccpp/lotoze-al-tools-cpp/core.cpp
@@ -0,0 +1,22 @@
+﻿// lotoze-al-tools-cpp.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "core.h"
+
+
+// 这是导出变量的一个示例
+LOTOZEALTOOLSCORE_API int nlotozealtoolscpp=0;
+
+// 这是导出函数的一个示例。
+LOTOZEALTOOLSCORE_API int fnlotozealtoolscpp(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+Clotozealtoolscpp::Clotozealtoolscpp()
+{
+    return;
+}
